bc6ee17513deec3bbbc78f659fd8d713c279ca0e
Naveen <78341076+DevSkugal@users.noreply.github.com>
2021-06-10 00:15:56 +0530

    commit 2

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types"
+
+export class Navbar extends Component {
+
+  static defaultProps={
+    icon:"fas fa-code-branch" ,
+    title:"Navbar"
+  }
+
+  static propTypes={
+    title:PropTypes.string.isRequired,
+    icon:PropTypes.string.isRequired
+  }
+
+
+  render() {
+    return (
+       <nav className="navbar bg-success">
+         <h1>
+             <i className={this.props.icon} /> {this.props.title}
+         </h1>
+       </nav>
+     
+    );
+  }
+}
+
+export default Navbar;
